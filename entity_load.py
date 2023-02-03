5bdd7142230b23ecbbac7d691630f7db8a6d377b
Chris <24978693+clally1@users.noreply.github.com>
2023-02-03 18:36:42 -0500

    Update entity_load.py

--- entity_load.py
+++ entity_load.py
@@ -7,8 +7,13 @@ isLocal=False
 import certifi
 import requests
 import json
+from urllib import parse
 import pymongo
-m_db=pymongo.MongoClient("mongodb+srv://chrislally:lLQg1YFf3Plj@fide-id.yslw4ye.mongodb.net/", tlsCAFile=certifi.where())
+import time
+import copy
+from bson.objectid import ObjectId
+import base64
+m_db=pymongo.MongoClient("mongodb+srv://$USER:$PASS@$DB.$KEY.mongodb.net/", tlsCAFile=certifi.where())
 
 def pubsub(topic_name,message):
     if isLocal:
@@ -42,12 +47,35 @@ def LOCALpubsub(topic_name,message):
 def fullEntity(fide_id):
     entity_final=m_db["entity_db"]["entities"].find_one({"fide_id":fide_id})
     
+    
+    if entity_final.get('times'):
+        entity_time=entity_final['times']
+        if entity_time.get('start'):
+            entity_time['start_str']=time.strftime('%m-%d-%Y', time.localtime(entity_time['start']))
+        
+        entity_final['times']=entity_time
+    
+    if entity_final.get('team_size'):
+        team_size=entity_final['team_size']
+        if team_size.get('count') and team_size.get('range') and team_size.get('range')!=0:
+            s_low=team_size.get('count')-team_size.get('range')
+            s_high=team_size.get('count')+team_size.get('range')
+            team_size['range_str']=str(s_low)+"-"+str(s_high)+" people"
+        elif team_size.get('count'):
+            team_size['range_str']=str(team_size.get('count'))+" people"
+        entity_final['team_size']=team_size
+    
     del entity_final['_id']
     del entity_final['time_updated']
     del entity_final['time_created']
     del entity_final['revision']
     
     return entity_final
+
+#%%
+# fide_id="f3365482381"
+# fullEntity(fide_id)
+#%%
 
 def entityLoad(request):
     request_json = request.get_json(silent=True)
